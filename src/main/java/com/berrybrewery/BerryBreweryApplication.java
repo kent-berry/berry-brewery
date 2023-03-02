e305b667be90cfa6f2bc456480210c66ad671db6
kentwberry4 <kentwberry@protonmail.com>
2023-03-02 10:43:42 -0500

    initial project add

---
+++ src/main/java/com/berrybrewery/BerryBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.berrybrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerryBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berrybrewery/BerryBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berrybrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerryBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
